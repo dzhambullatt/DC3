5640719c6a0c441f9b9806015581d74962eeea82
djambulat <jambulatahmadov99@gmail.com>
2022-09-29 18:08:56 +0300

    have some bags

--- django_movie/movies/admin.py
+++ django_movie/movies/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import *
+from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
 
 admin.site.register(Category)
 admin.site.register(Genre)
