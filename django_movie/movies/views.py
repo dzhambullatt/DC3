8c73fac4b51d2a807742811a2852129a3a8fd8bb
djambulat <jambulatahmadov99@gmail.com>
2022-09-30 18:53:41 +0300

    some changes

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -9,3 +9,9 @@ class MoviesView(View):
     def get(self, request):
         movies = Movie.objects.all()
         return render(request, "movies/movies.html", {"movie_list": movies})
+
+class MovieDetailView(View):
+    """Полное Описание Фильма"""
+    def get(self, request, pk):
+        movie = Movie.objects.get(id=pk)
+        return render(request, "movies/movie_detail.html", {"movie": movie})
