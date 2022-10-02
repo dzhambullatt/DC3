0256b1a8484d08d5b920f378f046908a1a676404
djambulat <jambulatahmadov99@gmail.com>
2022-10-02 20:34:45 +0300

    created new changes

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import View
 
 from .models import Movie
+from .forms import ReviewForm
 
 
 class MoviesView(View):
@@ -16,3 +17,16 @@ class MovieDetailView(View):
     def get(self, request, slug):
         movie = Movie.objects.get(url=slug)
         return render(request, "movies/movie_detail.html", {"movie": movie})
+
+
+class AddReview(View):
+    """Отзывы"""
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
+
